7bb082f1e6d38ca66576a0d71f9dbd086759a8ac
Ashwin Sankar <ashwins1211@gmail.com>
2025-03-18 12:20:13 +0530

    Add comments and docstrings to core.py

--- nn/core.py
+++ nn/core.py
@@ -4,29 +4,35 @@ from typing import TypedDict, Dict, List, Tuple
 import numpy as np
 
 class AutoDiff(ABC):
+    """Abstract base class for automatic differentiation."""
 
     def __init__(self) -> None:
-        self.cache: dict = {}
+        self.cache: dict = {}  # Stores intermediate values for backpropagation
 
     @abstractmethod
     def forward(self, *args) -> None:
+        """Performs the forward pass."""
         pass
 
     @abstractmethod
     def backward(self, *args) -> None:
+        """Computes gradients during backpropagation."""
         pass
 
     @abstractmethod
     def __call__(self, *args) -> None:
+        """Allows the class instance to be called as a function."""
         pass
 
 class Weights(TypedDict):
-    w: np.ndarray
-    b: np.ndarray
-    dw: np.ndarray
-    db: np.ndarray
+    """Defines the structure for layer weights and gradients."""
+    w: np.ndarray  # Weight matrix
+    b: np.ndarray  # Bias vector
+    dw: np.ndarray  # Gradient of weights
+    db: np.ndarray  # Gradient of biases
 
 class Layer(AutoDiff):
+    """Abstract base class for neural network layers."""
     weights: Weights = {}
 
     def __init__(self) -> None:
@@ -34,33 +40,40 @@ class Layer(AutoDiff):
 
     @abstractmethod
     def forward(self, *args) -> None:
+        """Computes the forward pass of the layer."""
         pass
 
     @abstractmethod
     def backward(self, *args) -> None:
+        """Computes the backward pass of the layer."""
         pass
 
     @abstractmethod
     def init_weights(self, *args) -> None:
+        """Initializes the weights of the layer."""
         pass
 
 class Loss(AutoDiff):
+    """Abstract base class for loss functions."""
 
     def __init__(self, model) -> None:
         super().__init__()
-        self.model = model
-        self.value: np.float32 = 0.0
-        self.reg_value: float = 0.0
+        self.model = model  # Reference to the model
+        self.value: np.float32 = 0.0  # Loss value
+        self.reg_value: float = 0.0  # Regularization value
 
     @abstractmethod
     def forward(self, *args) -> None:
+        """Computes the loss value."""
         pass
 
     @abstractmethod
     def diff(self, *args) -> None:
+        """Computes the derivative of the loss function."""
         pass
 
     def backward(self) -> None:
+        """Performs backpropagation through the loss function."""
         y_hat = self.diff()
         y_hat = self.model.output_activation.backward(y_hat)
         L = len(self.model.layers)
@@ -75,40 +88,50 @@ class Loss(AutoDiff):
         return self
     
 class Activation(AutoDiff):
+    """Abstract base class for activation functions."""
 
     def __init__(self) -> None:
         super().__init__()
-        self.cache['x'] = []
+        self.cache['x'] = []  # Stores input values for backpropagation
 
     @abstractmethod
     def forward(self, *args) -> None:
+        """Computes the forward pass of the activation function."""
         pass
 
     @abstractmethod
     def backward(self, *args) -> None:
+        """Computes the gradient of the activation function."""
         pass
 
     def __call__(self, *args) -> None:
         return self.forward(*args)
 
 class Optimizer(ABC):
+    """Abstract base class for optimization algorithms."""
+    
     def __init__(self) -> None:
-        self.params = {}
+        self.params = {}  # Stores parameters for optimization
 
     @abstractmethod
     def step(self, *args) -> None:
+        """Performs an optimization step."""
         pass
 
     @abstractmethod
     def zero_grad(self, *args) -> None:
+        """Resets gradients to zero."""
         pass
 
 class Module(ABC):
+    """Abstract base class for neural network models."""
+    
     @abstractmethod
     def __init__(self) -> None:
         pass
 
     def forward(self, x: np.ndarray) -> np.ndarray:
+        """Performs the forward pass through the model."""
         for layer in self.layers[:-1]:
             x = layer(x)
             x = self.activation(x)
@@ -117,14 +140,17 @@ class Module(ABC):
         return o
     
     def __call__(self, x) -> np.ndarray:
+        """Allows the model instance to be called as a function."""
         return self.forward(x)
     
     @property
     def parameters(self) -> List[Weights]:
+        """Returns a list of model parameters."""
         return [layer.weights for layer in self.layers]
     
     @property
     def state_dict(self) -> Dict[str, np.ndarray]:
+        """Returns the model's parameters as a dictionary."""
         state = {}
         for i, layer in enumerate(self.layers):
             state[f'layer_{i}_w'] = layer.weights['w']
@@ -132,6 +158,5 @@ class Module(ABC):
         return state
 
     def load_state_dict(self, state_dict: Dict[str, np.ndarray]) -> None:
+        """Loads model parameters from a dictionary."""
         for i, layer in enumerate(self.layers):
-            layer.weights['w'] = state_dict[f'layer_{i}_w']
-            layer.weights['b'] = state_dict[f'layer_{i}_b']
